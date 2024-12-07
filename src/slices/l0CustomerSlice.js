362df08f8e68ba9c701aec319d50df9580b75f92
anmol2609 <anmoldwivedi145@gmail.com>
2024-12-07 11:08:01 +0530

    customer list added

---
+++ src/slices/l0CustomerSlice.js
@@ -0,0 +1,54 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import { managementAxiosInstance } from 'src/config/Axios'
+//async function for get All Customers
+export const getL0Customer = createAsyncThunk(
+  'getL0Customer',
+  async (_, { rejectWithValue }) => {
+    try {
+      // Simulate an API call
+      const { data } = await managementAxiosInstance.get(`/user/all`)
+      return data.data // Return product data on success
+    } catch (error) {
+      return rejectWithValue(error.response.message)
+    }
+  },
+)
+
+// Slice setup
+const l0CustomersSlice = createSlice({
+  name: 'l0CustomersSlice',
+  initialState: {
+    l0_customers: [],
+    loading: false,
+    success: false,
+    error: null,
+  },
+  reducers: {
+    clearL0CustomersErrors: (state) => {
+      state.error = null
+      state.success = false
+    },
+  },
+  extraReducers: (builder) => { 
+    builder
+    //get All Data Centers
+    .addCase(getL0Customer.pending, (state) => {
+      state.loading = true
+    })
+    .addCase(getL0Customer.fulfilled, (state, action) => {
+      
+      state.loading = false
+      state.success = true
+      state.l0_customers = action.payload
+    })
+    .addCase(getL0Customer.rejected, (state, action) => {
+      state.loading = false
+      state.error = action.payload
+    })
+    
+  },
+})
+
+export const { clearL0CustomersErrors } = l0CustomersSlice.actions
+export const l0CustomersReducer = l0CustomersSlice.reducer
+
